b667a3a3acfda626e192fce263ea652bd6d0fc1d
aashish0770 <ssuzzan81@gmail.com>
2025-09-22 11:02:58 +0300

    Activity-2

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,74 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import SingleCounter from "./components/SingleCounter";
+import useField from "./components/useField";
+import "./App.css";
+import useLocalStorage from "./components/useLocalStorage";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  // part-1
+  const [counter, setCounter] = useState(0);
+
+  // part-2
+  const nameInput = useField("text");
+  const bornInput = useField("date");
+  const heightInput = useField("number");
+
+  // part-3
+  const [name, setName] = useLocalStorage("name", "");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("Submitted values:", {
+      name: nameInput.value,
+      born: bornInput.value,
+      height: heightInput.value,
+    });
+
+    localStorage.setItem("name", nameInput.value);
+    localStorage.setItem("born", bornInput.value);
+    localStorage.setItem("height", heightInput.value);
+  };
 
   return (
-    <>
+    <div className="app-container">
+      <div>{counter}</div>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <button onClick={() => setCounter(counter + 1)}>Plus</button>
+        <button onClick={() => setCounter(counter - 1)}>Minus</button>
+        <button onClick={() => setCounter(0)}>Reset</button>
+        <SingleCounter />
+        <SingleCounter />
+        <SingleCounter />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <div>
+            Name: <input {...nameInput} />
+          </div>
+          <br />
+          <div>
+            Birthdate: <input {...bornInput} />
+          </div>
+          <br />
+          <div>
+            Height: <input {...heightInput} />
+          </div>
+          <button type="submit">Submit</button>
+        </form>
+        <div>
+          {nameInput.value} {bornInput.value} {heightInput.value}
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <div>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <p>Your name is stored in localStorage: {name}</p>
+      </div>
+    </div>
+  );
+};
 
-export default App
+export default App;
